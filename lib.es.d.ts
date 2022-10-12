78feb23d36be013f2da93a86aa113ef20e5a8a9f
N <zhupengcheng5@163.com>
2022-10-12 16:24:39 +0800

    chore

--- lib.es.d.ts
+++ lib.es.d.ts
@@ -7,7 +7,7 @@ declare global {
      * @param o An object.
      * @param v A property name.
      */
-    hasOwn<T extends Record<keyof any, any>, K extends KeyofUnion<T>>(o: T, v: K): o is ExtractByKeys<T, K>
+    hasOwn<T extends AnyObject, K extends KeyofUnion<T>>(o: T, v: K): o is ExtractByKeys<T, K>
 
 
 
@@ -21,19 +21,17 @@ declare global {
      * Returns an array of index/substring of the string
      * @param s string
      */
-    entries<
-      T extends string
-    >(s: T): [`${number}`, string][]
+    entries(s: string): [`${number}`, string][]
 
-    /**
-     * Returns an array of key/values of the enumerable properties of an object
-     * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
-     */
-    entries<
-      ValueType,
-      Target extends AnyObject | ArrayLike<ValueType>,
-      StringKey extends ToString<keyof Target> = ToString<keyof Target>
-    >(o: Target): [StringKey, ValueType][]
+    // /**
+    //  * Returns an array of key/values of the enumerable properties of an object
+    //  * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
+    //  */
+    // entries<
+    //   ValueType,
+    //   Target extends AnyObject | ArrayLike<ValueType>,
+    //   StringKey extends ToString<keyof Target> = ToString<keyof Target>
+    // >(o: Target): [StringKey, ValueType][]
 
 
 
@@ -53,21 +51,21 @@ declare global {
      * Returns index string array
      * @param s string
      */
-    keys<T extends string>(s: T): `${number}`[]
+    keys(s: string): `${number}`[]
 
 
 
     /**
-     * Returns an array of string split
-     * @param o string
+     * Returns an empty array
+     * @param o number or symbol or bigint
      */
     values(o: number | symbol | bigint): []
 
     /**
      * Returns an array of string split
-     * @param o string
+     * @param s string
      */
-    values<T extends string>(o: T): Split<T>
+    values<T extends string>(s: T): Split<T>
   }
 
   namespace Reflect {
@@ -78,13 +76,13 @@ declare global {
      * @param target Object that contains the property on itself or in its prototype chain.
      * @param propertyKey Name of the property.
      */
-    function has<T extends Record<keyof any, any>, K extends keyof globalThis.Object>(target: T, propertyKey: K): target is T
+    function has<T extends AnyObject, K extends keyof globalThis.Object>(target: T, propertyKey: K): target is T
 
     /**
      * Equivalent to `propertyKey in target`.
      * @param target Object that contains the property on itself or in its prototype chain.
      * @param propertyKey Name of the property.
      */
-    function has<T extends Record<keyof any, any>, K extends KeyofUnion<T>>(target: T, propertyKey: K): target is ExtractByKeys<T, K>
+    function has<T extends AnyObject, K extends KeyofUnion<T>>(target: T, propertyKey: K): target is ExtractByKeys<T, K>
   }
 }
