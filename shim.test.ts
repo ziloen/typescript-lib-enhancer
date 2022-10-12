b7f617f9f25673fe6b9a764acdfce0c75dabe543
N <zhupengcheng5@163.com>
2022-10-12 16:19:26 +0800

    chore

--- shim.test.ts
+++ shim.test.ts
@@ -61,111 +61,122 @@ import type { Expect, Equal } from './utils'
 
 // Test struredClone()
 {
-  // Promise -> never
-  const clonePromise = structuredClone(new Promise(() => { }))
-  const cloneDeepPropmise = structuredClone({ a: { b: new Promise(() => { }) } })
+  {
+    // Promise -> never
+    const clonePromise = structuredClone(new Promise(() => { }))
+    const cloneDeepPropmise = structuredClone({ a: { b: new Promise(() => { }) } })
 
-  type TestCase = [
-    Expect<Equal<typeof clonePromise, never>>,
-    Expect<Equal<typeof cloneDeepPropmise, never>>,
-  ]
-}
+    type TestCase = [
+      Expect<Equal<typeof clonePromise, never>>,
+      Expect<Equal<typeof cloneDeepPropmise, never>>,
+    ]
+  }
 
-{
-  // Function -> never
-  const cloneFunction = structuredClone((a: string) => { })
-  const cloneDeepFunction = structuredClone({ a: { b: () => { } } })
+  {
+    // Function -> never
+    const cloneFunction = structuredClone((a: string) => { })
+    const cloneDeepFunction = structuredClone({ a: { b: () => { } } })
 
-  type TestCase = [
-    Expect<Equal<typeof cloneFunction, never>>,
-    Expect<Equal<typeof cloneDeepFunction, never>>,
-  ]
-}
+    type TestCase = [
+      Expect<Equal<typeof cloneFunction, never>>,
+      Expect<Equal<typeof cloneDeepFunction, never>>,
+    ]
+  }
 
-{
-  // Symbol -> never
-  const cloneSymbol = structuredClone({ a: Symbol() })
-  const cloneDeepSymbol = structuredClone({ a: { b: Symbol() } })
+  {
+    // Symbol -> never
+    const cloneSymbol = structuredClone({ a: Symbol() })
+    const cloneDeepSymbol = structuredClone({ a: { b: Symbol() } })
 
-  type TestCase = [
-    Expect<Equal<typeof cloneSymbol, never>>,
-    Expect<Equal<typeof cloneDeepSymbol, never>>,
-  ]
-}
+    type TestCase = [
+      Expect<Equal<typeof cloneSymbol, never>>,
+      Expect<Equal<typeof cloneDeepSymbol, never>>,
+    ]
+  }
 
-{
-  // Symbol key -> clone with out symbol key
-  const cloneSymbolKey = structuredClone({ [Symbol()]: 123, a: 12 })
-  const cloneDeepSymbolKey = structuredClone({ a: 12, b: { [Symbol()]: 123, c: 12 } })
+  {
+    // Symbol key -> clone with out symbol key
+    const cloneSymbolKey = structuredClone({ [Symbol()]: 123, a: 12 })
+    const cloneDeepSymbolKey = structuredClone({ a: 12, b: { [Symbol()]: 123, c: 12 } })
 
-  type TestCase = [
-    Expect<Equal<typeof cloneSymbolKey, { a: number }>>,
-    Expect<Equal<typeof cloneDeepSymbolKey, { a: number, b: { c: number } }>>,
-  ]
+    type TestCase = [
+      Expect<Equal<typeof cloneSymbolKey, { a: number }>>,
+      Expect<Equal<typeof cloneDeepSymbolKey, { a: number, b: { c: number } }>>,
+    ]
+  }
 }
 
 
 
 // Test Object.entries()
 {
-  // preserve key type, 
-  const entries = Object.entries({ a: 12, [Symbol()]: 99, 12: 33 })
+  {
+    // preserve key type, 
+    const entries = Object.entries({ a: 12, [Symbol()]: 99, 12: 33 })
 
-  type TestCase = [
-    Expect<Equal<typeof entries, ['a' | '12', number][]>>
-  ]
-}
-{
-  // primitive types
-  const entriesOfNumber = Object.entries(0)
-  const entriesOfString = Object.entries("123")
-  const entriesOfSymbol = Object.entries(Symbol())
+    type TestCase = [
+      Expect<Equal<typeof entries, ['a' | '12', number][]>>
+    ]
+  }
 
-  type TestCase = [
-    Expect<Equal<typeof entriesOfNumber, []>>,
-    Expect<Equal<typeof entriesOfString, [`${number}`, string][]>>,
-    Expect<Equal<typeof entriesOfSymbol, []>>,
-  ]
+  {
+    // primitive types
+    const entriesOfNumber = Object.entries(0)
+    const entriesOfString = Object.entries("123")
+    const entriesOfSymbol = Object.entries(Symbol())
+
+    type TestCase = [
+      Expect<Equal<typeof entriesOfNumber, []>>,
+      Expect<Equal<typeof entriesOfString, [`${number}`, string][]>>,
+      Expect<Equal<typeof entriesOfSymbol, []>>,
+    ]
+  }
 }
 
 
 
 // Test Object.keys()
 {
-  // symbol key ignored
-  const keys = Object.keys({ str: 12, 12: 99, [Symbol()]: 123 })
-  type Test = Expect<Equal<typeof keys, ("str" | "12")[]>>
-}
-{
-  // primitive types
-  const keysOfNumber = Object.keys(0)
-  const keysOfString = Object.keys("123")
-  const keysOfSymbol = Object.keys(Symbol())
+  {
+    // symbol key ignored
+    const keys = Object.keys({ str: 12, 12: 99, [Symbol()]: 123 })
+    type Test = Expect<Equal<typeof keys, ("str" | "12")[]>>
+  }
 
-  type TestCase = [
-    Expect<Equal<typeof keysOfNumber, []>>,
-    Expect<Equal<typeof keysOfString, `${number}`[]>>,
-    Expect<Equal<typeof keysOfSymbol, []>>
-  ]
+  {
+    // primitive types
+    const keysOfNumber = Object.keys(0)
+    const keysOfString = Object.keys("123")
+    const keysOfSymbol = Object.keys(Symbol())
+
+    type TestCase = [
+      Expect<Equal<typeof keysOfNumber, []>>,
+      Expect<Equal<typeof keysOfString, `${number}`[]>>,
+      Expect<Equal<typeof keysOfSymbol, []>>
+    ]
+  }
 }
 
 
 
 // Test Object.values()
 {
-  // symbol key value ignored
-  const values = Object.values({ str: 12, 12: 99, [Symbol()]: 123 })
-  type Test = Expect<Equal<typeof values, number[]>>
-}
-{
-  // primitive types
-  const valuesOfNumber = Object.values(0)
-  const valuesOfString = Object.values("123")
-  const valuesOfSymbol = Object.values(Symbol())
+  {
+    // symbol key value ignored
+    const values = Object.values({ str: 12, 12: 99, [Symbol()]: 123 })
+    type Test = Expect<Equal<typeof values, number[]>>
+  }
 
-  type TestCase = [
-    Expect<Equal<typeof valuesOfNumber, []>>,
-    Expect<Equal<typeof valuesOfString, ["1", "2", "3"]>>,
-    Expect<Equal<typeof valuesOfSymbol, []>>
-  ]
+  {
+    // primitive types
+    const valuesOfNumber = Object.values(0)
+    const valuesOfString = Object.values("123")
+    const valuesOfSymbol = Object.values(Symbol())
+
+    type TestCase = [
+      Expect<Equal<typeof valuesOfNumber, []>>,
+      Expect<Equal<typeof valuesOfString, ["1", "2", "3"]>>,
+      Expect<Equal<typeof valuesOfSymbol, []>>
+    ]
+  }
 }
