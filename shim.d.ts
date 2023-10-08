cc844a2da2f5a42790fa4d53835207bc58e0d6f1
N <34268416+ziloen@users.noreply.github.com>
2023-10-08 15:37:18 +0800

    chore: add KeyboardEvent.key and InputEvent.inputType

--- shim.d.ts
+++ shim.d.ts
@@ -1,3 +1,6 @@
-import "typed-query-selector/strict"
-import "./lib.dom"
-import "./lib.es"
+import 'typed-query-selector/strict'
+import './lib.dom'
+import './lib.es'
+
+export type { KeyboardEventKey } from './KeyboardEventKey'
+export type { InputEventInputType } from './InputEventInputType'
